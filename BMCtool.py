ce9f3f93fd8eca0cba1206fe8d21d9d99a885bad
lixianfa <lixianfa.official@gmail.com>
2019-11-20 09:56:07 +0800

    test success

--- BMCtool.py
+++ BMCtool.py
@@ -1,4 +1,5 @@
-#!/bin/env python
+#coding=utf-8
+#!/usr/bin/env python
 
 import json
 import sys
@@ -9,13 +10,18 @@ host = '192.168.2.63'
 user = 'Administrator'
 passwd = 'Admin@9000'
 operation = 'sdr'
-sdr_promote = "FAN4 Presence"
+sdr_promote = 'FAN4 Presence'
 
-with open('ipmitool.log', 'w') as fout:
+with open('ipmitool.log', 'a+') as fout:
+    print('ipmitool -I %s -H %s -U %s -P %s %s' % (lanplus, host, user, passwd, operation))
     child = pexpect.spawn('ipmitool -I %s -H %s -U %s -P %s %s' % (lanplus, host, user, passwd, operation))
-    child.logfile = fout
+    child.logfile = fout.buffer
     # 将pexpect的输入输出信息输出到标准输出
-    # child.logfile = sys.stdout
-    index = child.expect([sdr_promote, pexpect.EOF, pexpect.TIMEOUT])
-
-    print(index)
+    # child.logfile = sys.stdout.buffer
+    index = child.expect(['FAN4 Presence'])
+    print(child.before.decode())
+    print('------------------')
+    print(str(child.after))
+    print(str(index))
+    fout.close()
+    
